960de0f0460dbe973e4a8251cdc09fed5dc5293b
Matt Davey <matt.davey@gmail.com>
2012-07-11 22:36:32 -0400

    Added a MarketOrder cucumber test

--- src/test/java/com/euronextclone/ContinuousMatchingStepDefinitions.java
+++ src/test/java/com/euronextclone/ContinuousMatchingStepDefinitions.java
@@ -1,5 +1,6 @@
 package com.euronextclone;
 
+import com.euronextclone.ordertypes.MarketOrder;
 import com.euronextclone.ordertypes.MarketToLimit;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
@@ -7,12 +8,6 @@ import cucumber.annotation.en.Then;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-/**
- * Created with IntelliJ IDEA.
- * User: eprystupa
- * Date: 7/11/12
- * Time: 9:54 AM
- */
 public class ContinuousMatchingStepDefinitions {
     private final MatchingUnit matchingUnit = new MatchingUnit();
 
@@ -26,6 +21,16 @@ public class ContinuousMatchingStepDefinitions {
     public void the_MTL_sell_order_from_broker_for_shares(String broker, int quantity) throws Throwable {
 
         matchingUnit.newOrder(Order.OrderSide.Sell, broker, quantity, new OrderPrice(MarketToLimit.INSTANCE));
+    }
+
+    @Given("^the MO buy order from broker \"([^\"]*)\" for (\\d+) shares$")
+    public void the_MO_buy_order_from_broker_for_shares(String broker, int quantity) throws Throwable {
+        matchingUnit.newOrder(Order.OrderSide.Buy, broker, quantity, new OrderPrice(MarketOrder.INSTANCE));
+    }
+
+    @Given("^the Limit sell order from broker \"([^\"]*)\" for (\\d+) shares$")
+    public void the_Limit_sell_order_from_broker_for_shares(String broker, int quantity) throws Throwable {
+        matchingUnit.newOrder(Order.OrderSide.Sell, broker, quantity, new OrderPrice(MarketOrder.INSTANCE));
     }
 
     @Then("^remaining buy order book depth is (\\d+)$")
--- src/test/resources/com/euronextclone/continuous-market.feature
+++ src/test/resources/com/euronextclone/continuous-market.feature
@@ -4,3 +4,11 @@ Feature: Matching Rules in continuous matching mode
     And the MTL sell order from broker "B" for 40 shares
     Then remaining buy order book depth is 1
     And remaining sell order book depth is 0
+
+  Scenario: Continuous mode call phase with  Market to limits, Pure Market orders and limited orders
+    Given the MTL buy order from broker "A" for 10 shares
+    And the MO buy order from broker "B" for 10 shares
+    And the MTL buy order from broker "C" for 10 shares
+    And the Limit sell order from broker "D" for 10 shares
+    Then remaining buy order book depth is 2
+    And remaining sell order book depth is 0
