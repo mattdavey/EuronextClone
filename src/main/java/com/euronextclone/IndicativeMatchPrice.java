349f6910622d233bc8dd6413cfba1db82a3ccf4c
Stepi <unsteppi@gmail.com>
2012-07-17 11:33:02 +0100

    fixed sell side book ordering

--- src/main/java/com/euronextclone/IndicativeMatchPrice.java
+++ src/main/java/com/euronextclone/IndicativeMatchPrice.java
@@ -1,27 +1,40 @@
 package com.euronextclone;
 
-public class IndicativeMatchPrice {
+public class IndicativeMatchPrice
+{
     private OrderTypeLimit orderTypeLimit;
     private int quantity;
 
-    public IndicativeMatchPrice(final Order.OrderSide side) {
-        orderTypeLimit = new OrderTypeLimit(OrderType.Limit);
+    public IndicativeMatchPrice(final Order.OrderSide side)
+    {
+        if (side == Order.OrderSide.Buy)
+        {
+            orderTypeLimit = new OrderTypeLimit(OrderType.Limit);
+        }
+        else
+        {
+            orderTypeLimit = new OrderTypeLimit(OrderType.Limit, 0);
+        }
     }
 
-    public void reset() {
-        quantity=0;
+    public void reset()
+    {
+        quantity = 0;
 //        orderTypeLimit.update(0);
     }
 
-    public void addQuantity(final int quantity) {
+    public void addQuantity(final int quantity)
+    {
         this.quantity += quantity;
     }
 
-    public int getQuantity() {
+    public int getQuantity()
+    {
         return quantity;
     }
 
-    public OrderTypeLimit getOrderPrice() {
+    public OrderTypeLimit getOrderPrice()
+    {
         return orderTypeLimit;
     }
 }
